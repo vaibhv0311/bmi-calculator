c739f9847dee1f7eb02b908efe2f44a47e453758
vaibhav0311 <sharma.vaibhav0311@gmail.com>
2022-01-24 20:31:57 +0530

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(theme: ThemeData(), home: BmiCalculator());
+  }
+}
+
+class BmiCalculator extends StatefulWidget {
+  const BmiCalculator({Key? key}) : super(key: key);
+
+  @override
+  _BmiCalculatorState createState() => _BmiCalculatorState();
+}
+
+class _BmiCalculatorState extends State<BmiCalculator> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
