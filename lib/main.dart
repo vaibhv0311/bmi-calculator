d6ac697e7f4c012fa02b06be97df2fd68041bf92
vaibhav0311 <sharma.vaibhav0311@gmail.com>
2022-01-27 20:54:14 +0530

    New File for base input layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'inputpage.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,32 +15,7 @@ class MyApp extends StatelessWidget {
         primaryColor: Color(0xFF0A0E21),
         scaffoldBackgroundColor: Color(0xFF0A0E21),
       ),
-      home: BmiCalculator(),
-    );
-  }
-}
-
-class BmiCalculator extends StatefulWidget {
-  const BmiCalculator({Key? key}) : super(key: key);
-
-  @override
-  _BmiCalculatorState createState() => _BmiCalculatorState();
-}
-
-class _BmiCalculatorState extends State<BmiCalculator> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('BMI Calculator'),
-      ),
-      body: Center(
-        child: Text('Body Text'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: (){},
-      ),
+      home: InputPage(),
     );
   }
 }
